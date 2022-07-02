e8c3332d81cd4de152d8de72baacc880b228cb04
tair12 <tair-00@list.ru>
2022-07-02 09:45:46 +0300

    dz1

--- dz 1/app.js
+++ dz 1/app.js
@@ -1,14 +1,28 @@
-const  innInput = document.querySelector(".innInput");
-const  innCheck = document.querySelector(".innCheck");
-const  innResult = document.querySelector(".innResult");
-  const innRegExp =/\ 0 \d{13}/;
+// const  innInput = document.querySelector(".innInput");
+// const  innCheck = document.querySelector(".innCheck");
+// const  innResult = document.querySelector(".innResult");
+//   const innRegExp =/\ 0 \d{13}/;
 
-  innCheck.addEventListener("click",()=>{
+//   innCheck.addEventListener("click",()=>{
    
 
-    if(innRegExp.test(innInput.value )){
-      console.log( "ok")
-      }else {
-        console.log( "not ok")
-    }
-  });
+//     if(innRegExp.test(innInput.value )){
+//       console.log( "ok")
+//       }else {
+//         console.log( "not ok")
+//     }
+//   });
+ const block = document.querySelector(".block");
+ const move = document.querySelector(".move");
+ let position = 0;
+ const blockMove = ()=>{
+   if (position < 460) {
+      position += 15;
+      block.style.left =`${position}px `
+}
+ }
+
+ move.addEventListener ("clck" () => {
+   blockMove();
+ })
+
